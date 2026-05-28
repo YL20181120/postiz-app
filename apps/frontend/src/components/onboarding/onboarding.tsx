26c80193ff4b5ceecbc5d7ef0732ccd7b742e3cb
丶大毛 <youjingqiang@vip.qq.com>
2026-05-28 12:43:23 +0800

    change name
    # Conflicts:
    #	libraries/nestjs-libraries/src/integrations/social/threads.provider.ts

--- apps/frontend/src/components/onboarding/onboarding.tsx
+++ apps/frontend/src/components/onboarding/onboarding.tsx
@@ -32,7 +32,7 @@ export const Onboarding: FC = () => {
     }
     modalOpen.current = true;
     modal.openModal({
-      // title: t('onboarding', 'Welcome to Postiz'),
+      // title: t('onboarding', 'Welcome to Veloop'),
       withCloseButton: true,
       closeOnEscape: false,
       removeLayout: true,
